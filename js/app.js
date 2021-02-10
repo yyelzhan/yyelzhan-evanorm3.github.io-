68baf42fe756985830f7e97b0d81453e5cf2b273
yyelzhan <62288428+yyelzhan@users.noreply.github.com>
2021-02-10 17:23:52 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    slides = document.querySelectorAll('.mySlides'),
+    dots = document.querySelectorAll('.dot');
+
+
+let index = 0;
+
+const prepareCurrentSlide = (ind) => {
+    activeSlide(ind);
+    activeDot(ind);
+}
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('active')
+    }
+    slides[n].classList.add('active')
+}
+
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('active')
+    }
+    dots[n].classList.add('active')
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+}
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    });
+});
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+})
